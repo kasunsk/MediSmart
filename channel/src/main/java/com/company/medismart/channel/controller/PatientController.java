05e2ceca1e056a2e586b513602982547170f5a9e
Kasun Kariyawasam <kasunsk@gmail.com>
2019-12-18 02:28:48 +0800

    Basic changes

---
+++ channel/src/main/java/com/company/medismart/channel/controller/PatientController.java
@@ -0,0 +1,12 @@
+package com.company.medismart.channel.controller;
+
+import com.company.medismart.channel.dto.Patient;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PatientController {
+
+    public void addNewPatient(Patient patient) {
+
+    }
+}
---
+++ channel/src/main/java/com/company/medismart/channel/dto/Patient.java
@@ -0,0 +1,86 @@
+package com.company.medismart.channel.dto;
+
+public class Patient {
+
+    private String nic;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private Boolean diabetes;
+    private Boolean bloodPressure;
+    private Boolean cholesterol;
+    private Boolean allergy;
+    private String specialNote;
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Boolean getDiabetes() {
+        return diabetes;
+    }
+
+    public void setDiabetes(Boolean diabetes) {
+        this.diabetes = diabetes;
+    }
+
+    public Boolean getBloodPressure() {
+        return bloodPressure;
+    }
+
+    public void setBloodPressure(Boolean bloodPressure) {
+        this.bloodPressure = bloodPressure;
+    }
+
+    public Boolean getCholesterol() {
+        return cholesterol;
+    }
+
+    public void setCholesterol(Boolean cholesterol) {
+        this.cholesterol = cholesterol;
+    }
+
+    public Boolean getAllergy() {
+        return allergy;
+    }
+
+    public void setAllergy(Boolean allergy) {
+        this.allergy = allergy;
+    }
+
+    public String getSpecialNote() {
+        return specialNote;
+    }
+
+    public void setSpecialNote(String specialNote) {
+        this.specialNote = specialNote;
+    }
+}
