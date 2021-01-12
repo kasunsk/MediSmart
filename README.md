56b533d1a05a0a7fec6d116acb9c1e61ceb8bed9
Kasun <kasunsk@gmail.com>
2021-01-12 23:50:17 +0800

    Adding docker and swagger ui

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # MediSmart
 
 This system will help doctors to keep history data of patients.
+
+http://localhost:8282/swagger-ui.html
--- channel/src/main/java/com/company/medismart/channel/ChannelApplication.java
+++ channel/src/main/java/com/company/medismart/channel/ChannelApplication.java
@@ -1,11 +1,13 @@
 package com.company.medismart.channel;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableTransactionManagement
